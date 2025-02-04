187c2d4d2177c853ead1816cf47dc3a0c96ba8d1
Martin Bjeldbak Madsen <me@martinbjeldbak.com>
2025-02-05 02:05:37 +1100

    fix: set cluster, getting perm error in controller

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -11,7 +11,6 @@ resource "proxmox_virtual_environment_user" "kubernetes_csi" {
     path = "/"
     propagate = true
     role_id =  proxmox_virtual_environment_role.csi.role_id
-
   }
   user_id = "kubernetes-csi@pve"
 }
