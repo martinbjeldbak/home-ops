ea262ba09e4f442754db7b124212ee4e7528c743
Martin Bjeldbak Madsen <me@martinbjeldbak.com>
2025-02-05 02:33:35 +1100

    fix: restore names, and grant permissions to token

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -19,5 +19,6 @@ resource "proxmox_virtual_environment_user_token" "kubernetes_csi" {
   comment         = "Managed by OpenTofu"
   expiration_date = "2033-01-01T22:00:00Z"
   token_name      = "csi"
+  privileges_separation = false # token has same permission as user
   user_id         = proxmox_virtual_environment_user.kubernetes_csi.user_id
 }
