18723214e0329b08f96b0863233da643788df645
Martin Bjeldbak Madsen <me@martinbjeldbak.com>
2025-02-08 21:24:46 +1100

    feat: create homepage user and group in proxmox

--- terraform/main.tf
+++ terraform/main.tf
@@ -13,6 +13,7 @@ resource "proxmox_virtual_environment_user" "kubernetes_csi" {
     role_id =  proxmox_virtual_environment_role.csi.role_id
   }
   user_id = "kubernetes-csi@pve"
+  comment = "Managed by OpenTofu"
 }
 
 resource "proxmox_virtual_environment_user_token" "kubernetes_csi" {
@@ -30,4 +31,33 @@ resource "proxmox_virtual_environment_acme_dns_plugin" "cloudflare_dns" {
     CF_Email= var.proxmox_cloudflare_email
     CF_Token=var.proxmox_cloudflare_token
   }
+}
+
+resource "proxmox_virtual_environment_group" "homepage" {
+  comment  = "Managed by OpenTofu"
+  group_id = "homepage"
+}
+
+resource "proxmox_virtual_environment_acl" "homepage_group_acl" {
+  group_id =  proxmox_virtual_environment_group.homepage.group_id
+  role_id = "PVEAuditor"
+
+  path      = "/"
+  propagate = true
+}
+
+resource "proxmox_virtual_environment_user" "homepage" {
+  user_id = "homepage@pve"
+  comment = "Managed by OpenTofu"
+  groups = [
+    proxmox_virtual_environment_group.homepage.group_id
+  ]
+}
+
+resource "proxmox_virtual_environment_user_token" "homepage" {
+  comment         = "Managed by OpenTofu"
+  expiration_date = "2033-01-01T22:00:00Z"
+  token_name      = "homepage"
+  privileges_separation = false # token has same permission as user
+  user_id         = proxmox_virtual_environment_user.homepage.user_id
 }
--- terraform/output.tf
+++ terraform/output.tf
@@ -3,3 +3,8 @@ output "kubernetes_csi_user_token" {
 
 value = proxmox_virtual_environment_user_token.kubernetes_csi.value
 }
+
+output "homepage_user_token" {
+  sensitive = true
+  value = proxmox_virtual_environment_user_token.homepage.value
+}
